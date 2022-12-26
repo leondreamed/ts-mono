721175d30a94df585cfac5408ffa1cb76bf51078
Leon Si <leon@leondreamed.com>
2022-12-25 23:28:04 -0500

    feat: export helpers

---
+++ packages/tsmr/src/index.ts
@@ -0,0 +1,9 @@
+export { turboLint, turboTypecheck } from './utils/turbo.js'
+export {
+	buildTypecheckFolder,
+	deleteCachedLintFiles,
+	deleteCachedTypecheckFiles,
+	setupLintAndTypecheck,
+	turboBuildTypecheckFolders,
+	typecheck,
+} from './utils/typecheck.js'
